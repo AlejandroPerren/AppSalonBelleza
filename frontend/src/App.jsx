2009f245a2984d1b7e6e6e03d05487bbd13e95fa
Alejandro <aletomasperren@gmail.com>
2024-10-23 13:03:46 -0300

    init proyect

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-50'>Hola
+      </div>
+    </>
+  )
+}
+
+export default App

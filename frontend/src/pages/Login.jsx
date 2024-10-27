5e4a936bf8686c9982bd30d33081cd5fa22c434f
Alejandro <aletomasperren@gmail.com>
2024-10-27 20:29:41 -0300

    Finish Auth desing

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,36 +4,36 @@ import { Link } from 'react-router-dom'
 const Login = () => {
   return (
     <div className='grid grid-cols-2 h-screen'>
-    <div className='flex item-center justify-center'>
-        <img src="../../public/1.jpg" className='w-full h-full object-cover'/>
-    </div>
+      <div className='flex item-center justify-center'>
+        <img src="../../public/1.jpg" className='w-full h-full object-cover' />
+      </div>
 
-    <div >
-      <div className='text-center'>
-      <h1 className='font-extrabold mt-16 '>Login</h1>
-      <p className='mt-12'>Inicia sesion con tus datos</p>
-      </div>
-      <div>
-      <form>
-        <div className='grid grid-cols-2 my-12'>
-              <label htmlFor="email">E-mail</label>
-              <input className='' type="text"/>
-   
-              <label htmlFor="Contraseña">Contraseña</label>
-              <input className='' type="password"/>
+      <div className='mx-6' >
+        <div className='text-center'>
+          <h1 className='font-extrabold mt-36 text-5xl'>Login</h1>
+          <p className='mt-24'>Inicia sesion con tus datos</p>
         </div>
-        <button type='submit'>
-            Ingresa
-          </button>
-        
-      </form>
-      <div className='justify-between'>
-          <Link to="forgotPass">¿Olvidaste tu Password?</Link>
-          <Link to="login">Aun no tienes una cuenta? Crea Una</Link>
+        <div>
+          <form>
+            <div className='grid grid-cols-customAuth my-16 gap-6'>
+              <label htmlFor="email">Email</label>
+              <input className='rounded-lg py-2 placeholder:pl-4' type="text" placeholder='Tu Email' />
+
+              <label htmlFor="Contraseña">Contraseña</label>
+              <input className='rounded-lg py-2 placeholder:pl-4' type="password" placeholder='Tu Contraseña'/>
+            </div>
+            <button type='submit' className='bg-azul mt-6 py-4 px-10 font-extrabold  hover:bg-blue-600'>
+              Iniciar Sesión
+            </button>
+
+          </form>
+          <div className='flex justify-between my-28'>
+            <Link to="forgotPass">¿Olvidaste tu Password?</Link>
+            <Link to="/register">Aun no tienes una cuenta? Crea Una</Link>
+          </div>
         </div>
       </div>
     </div>
-</div>
   )
 }
 

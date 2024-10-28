40d35f0f53b8bfd29c3c388177bd286a9851bb7d
Alejandro <aletomasperren@gmail.com>
2024-10-28 10:16:21 -0300

    Cors intregate

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const { route } = require('./routes/routes')
 const dotenv = require('dotenv').config
-
+const cors = require('cors')
 
 const app = express()
+var corsOptions = {
+    origin: 'http://localhost:5173.com',
+    optionsSuccessStatus: 200 
+  }
 
-app.use('/api', route)
+app.use('/api',cors(corsOptions) ,route)
 
 PORT = 4000 || process.env.PORT
 app.listen(PORT, () =>{
